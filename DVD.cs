ad775241d28cbe69fb078fb9b1458eb79f1d487d
Sara Terrier <117436275+SaraTerrier@users.noreply.github.com>
2024-09-19 19:41:59 +0200

    Add files via upload

---
+++ DVD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Biblio_Media
+{
+    internal class DVD : Media
+    {
+        double v_duree;
+        string v_realisateur;
+
+        public void AfficherInfos()
+        {
+            base.AfficherInfos();
+            Console.WriteLine(v_duree);
+            Console.WriteLine(v_realisateur);
+        }
+
+        public DVD(string v_titre, int v_numRef, int v_nbExemDispo, double v_duree, string v_realisateur) : base(v_titre, v_numRef, v_nbExemDispo)
+        {
+            this.v_duree = v_duree;
+            this.v_realisateur = v_realisateur;
+        }
+    }
+}
---
+++ Livre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Biblio_Media
+{
+    internal class Livre : Media
+    {
+        string v_auteur;
+        string v_genre;
+
+        public void AfficherInfos()
+        {
+            base.AfficherInfos();
+            Console.WriteLine(v_auteur);
+            Console.WriteLine(v_genre);
+        }
+
+        public Livre(string v_titre, int v_numRef, int v_nbExemDispo, string v_auteur, string v_genre) : base(v_titre, v_numRef, v_nbExemDispo)
+        {
+    
+            this.v_auteur = v_auteur;
+            this.v_genre = v_genre;
+        }
+    }
+}
